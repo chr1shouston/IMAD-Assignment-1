05f6cb7b3233dadb59a9060e189e7b6722c7d897
chr1shouston <st10536381@myemeris.edu.za>
2026-03-13 10:44:57 +0200

    Added if else statements instead
    The assignment says to use if statements  so I got rid of my 1 when statement and replaced it with 40 lines of code that does the same thing

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-
+    // Declaring variables
     private lateinit var btnMakeSpark: Button
     private lateinit var tvSparkResult: TextView
     private lateinit var edtTimeOfDay: EditText
@@ -21,17 +21,70 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // Typecasting
         btnMakeSpark = findViewById(R.id.btnMakeSpark)
         tvSparkResult = findViewById(R.id.tvSparkResult)
         edtTimeOfDay = findViewById(R.id.edtTimeOfDay)
 
 
-            btnMakeSpark.setOnClickListener {
-                when (edtTimeOfDay.text.toString()) {
-                    "Morning" -> tvSparkResult.text = "morning spark"
-                else -> tvSparkResult.text = "please enter something else"}
-            }
+        // Defining sparks for different times of day
+        val morningSpark = "Send a 'Good morning' text to a family member!"
+        val midMorningSpark = "Reach out to a colleague with a quick 'Thank you!'"
+        val afternoonSpark = "Share a funny Meme or interesting link with a friend!"
+        val afternoonSnackTimeSpark = "Send a quick 'Thinking of you' message!"
+        val dinnerSpark = "Call a friend or relative for a 5-minute catch-up!"
+        val nightSpark = "Leave a thoughtful comment on a friend's post"
 
+        // setOnClickListener (executes the following code upon button being pressed)
+        btnMakeSpark.setOnClickListener {
+            try {
+                if (edtTimeOfDay.text.toString().lowercase() == "morning"){
+                    tvSparkResult.text = morningSpark
+                }
+                else {
+                    tvSparkResult.text = "Please enter a valid time of day"
+                }
+                if (edtTimeOfDay.text.toString().lowercase() == "mid morning" || "mid-morning"){
+                    tvSparkResult.text = midMorningSpark
+                }
+                else {
+                    tvSparkResult.text = "Please enter a valid time of day"
+                }
+                if (edtTimeOfDay.text.toString().lowercase() == "morning"){
+                    tvSparkResult.text = morningSpark
+                }
+                else {
+                    tvSparkResult.text = "Please enter a valid time of day"
+                }
+                if (edtTimeOfDay.text.toString().lowercase() == "morning"){
+                    tvSparkResult.text = morningSpark
+                }
+                else {
+                    tvSparkResult.text = "Please enter a valid time of day"
+                }
+                if (edtTimeOfDay.text.toString().lowercase() == "morning"){
+                    tvSparkResult.text = morningSpark
+                }
+                else {
+                    tvSparkResult.text = "Please enter a valid time of day"
+                }
+                if (edtTimeOfDay.text.toString().lowercase() == "morning"){
+                    tvSparkResult.text = morningSpark
+                }
+                else {
+                    tvSparkResult.text = "Please enter a valid time of day"
+                }
+                if (edtTimeOfDay.text.toString().lowercase() == "morning"){
+                    tvSparkResult.text = morningSpark
+                }
+                else {
+                    tvSparkResult.text = "Please enter a valid time of day"
+                }
+
+
+            } catch {
+
+            }
 
             ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
                 val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -40,3 +93,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+}
